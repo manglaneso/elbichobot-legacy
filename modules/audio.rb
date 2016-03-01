8e16f02b450136d117e16fd7c12728545f1ecc33
Fayán Leonardo Pardo Ladino <Leopardo@MacBook-Pro-de-Fayan.local>
2016-03-01 20:47:07 +0100

    modified bicho to audio module, added siuu

--- modules/audio.rb
+++ modules/audio.rb
@@ -3,3 +3,7 @@
 def bicho(bot, message)
   bot.api.send_audio(chat_id: message.chat.id, audio: File.new('../audio/bicho.ogg'))
 end
+
+def siuu(bot, message)
+  bot.api.send_audio(chat_id: message.chat.id, audio: File.new('../audio/siuuu.ogg'))
+end
