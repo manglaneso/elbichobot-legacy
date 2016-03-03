a5cd8e5e52f44e95d99edf3bf70494ec1ac2b21d
manglasape <100303534@alumnos.uc3m.es>
2016-03-03 01:38:52 +0100

    added jaj, converted siuu to ogg

--- modules/audio.rb
+++ modules/audio.rb
@@ -5,5 +5,9 @@ def bicho(bot, message)
 end
 
 def siuu(bot, message)
-  bot.api.send_audio(chat_id: message.chat.id, audio: File.new('../audio/siuuu.mp3'))
+  bot.api.send_audio(chat_id: message.chat.id, audio: File.new('../audio/siuuu.ogg'))
+end
+
+def jaj(bot, message)
+  bot.api.send_audio(chat_id: message.chat.id, audio: File.new('../audio/jaj.ogg'))
 end
